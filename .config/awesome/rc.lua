3e2db5b3bfc49e0d8ea23c7e1c9b5b270df4c9c4
Mason Larobina <mason.larobina@gmail.com>
2011-06-17 09:10:29 +0800

    Only put window borders on URxvt

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -300,17 +300,17 @@ root.keys(globalkeys)
 awful.rules.rules = {
     -- All clients will match this rule.
     { rule = { },
-      properties = { border_width = beautiful.border_width,
+      properties = { border_width = 0,
                      border_color = beautiful.border_normal,
                      focus = true,
                      keys = clientkeys,
                      buttons = clientbuttons } },
     { rule = { class = "MPlayer" },
       properties = { floating = true } },
-    { rule = { class = "pinentry" },
-      properties = { floating = true } },
     { rule = { class = "gimp" },
       properties = { floating = true } },
+    { rule = { class = "URxvt" },
+      properties = { border_width = beautiful.border_width } },
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { tag = tags[1][2] } },
