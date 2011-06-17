6678e975137ea4fd1e1ff81c999b33e28b8acf69
Mason Larobina <mason.larobina@gmail.com>
2011-06-17 09:11:27 +0800

    Move to darker awesome theme colours & use dark layout icons

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -4,22 +4,21 @@
 
 theme = {}
 
-theme.font          = "Terminus 5"
+theme.font          = "Terminus 9"
 
-theme.bg_normal     = "#222222"
-theme.bg_focus      = "#535d6c"
-theme.bg_urgent     = "#ff0000"
+theme.bg_normal     = "#151515"
+theme.bg_focus      = "#202020"
+theme.bg_urgent     = "#D23D3D"
 theme.bg_minimize   = "#444444"
 
-theme.fg_normal     = "#aaaaaa"
-theme.fg_focus      = "#ffffff"
+theme.fg_normal     = "#555555"
+theme.fg_focus      = "#dddddd"
 theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
 
 theme.border_width  = "3"
-theme.border_normal = "#ffffff"
--- theme.border_focus  = "#535d6c"
-theme.border_focus  = "#ffffff"
+theme.border_normal = "#303030"
+theme.border_focus  = "#E84F4F"
 theme.border_marked = "#ffffff"
 
 -- There are other variable sets
@@ -78,20 +77,20 @@ theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/defau
 theme.wallpaper_cmd = { "feh --bg-tile /home/mason/.local/share/awesome/bg/dark_jeans.png" }
 
 -- You can use your own layout icons like this:
-theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
-theme.layout_fairv = "/usr/share/awesome/themes/default/layouts/fairvw.png"
-theme.layout_floating  = "/usr/share/awesome/themes/default/layouts/floatingw.png"
-theme.layout_magnifier = "/usr/share/awesome/themes/default/layouts/magnifierw.png"
-theme.layout_max = "/usr/share/awesome/themes/default/layouts/maxw.png"
-theme.layout_fullscreen = "/usr/share/awesome/themes/default/layouts/fullscreenw.png"
-theme.layout_tilebottom = "/usr/share/awesome/themes/default/layouts/tilebottomw.png"
-theme.layout_tileleft   = "/usr/share/awesome/themes/default/layouts/tileleftw.png"
-theme.layout_tile = "/usr/share/awesome/themes/default/layouts/tilew.png"
-theme.layout_tiletop = "/usr/share/awesome/themes/default/layouts/tiletopw.png"
-theme.layout_spiral  = "/usr/share/awesome/themes/default/layouts/spiralw.png"
-theme.layout_dwindle = "/usr/share/awesome/themes/default/layouts/dwindlew.png"
+theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairh.png"
+theme.layout_fairv = "/usr/share/awesome/themes/default/layouts/fairv.png"
+theme.layout_floating  = "/usr/share/awesome/themes/default/layouts/floating.png"
+theme.layout_magnifier = "/usr/share/awesome/themes/default/layouts/magnifier.png"
+theme.layout_max = "/usr/share/awesome/themes/default/layouts/max.png"
+theme.layout_fullscreen = "/usr/share/awesome/themes/default/layouts/fullscreen.png"
+theme.layout_tilebottom = "/usr/share/awesome/themes/default/layouts/tilebottom.png"
+theme.layout_tileleft   = "/usr/share/awesome/themes/default/layouts/tileleft.png"
+theme.layout_tile = "/usr/share/awesome/themes/default/layouts/tile.png"
+theme.layout_tiletop = "/usr/share/awesome/themes/default/layouts/tiletop.png"
+theme.layout_spiral  = "/usr/share/awesome/themes/default/layouts/spiral.png"
+theme.layout_dwindle = "/usr/share/awesome/themes/default/layouts/dwindle.png"
 
 theme.awesome_icon = "/usr/share/awesome/icons/awesome16.png"
 
 return theme
--- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=80
+-- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
