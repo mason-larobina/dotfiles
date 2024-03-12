e270dc9657c3b5deadfeab0f9a291b85d6b13036
Mason Larobina <mason.larobina@pm.me>
2024-03-13 05:19:18 +0800

    Update link.sh

--- link.sh
+++ link.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/zsh
 
-set -xe
+DOTFILES=~/.dotfiles
 
-DOTFILES_ROOT=~/.dotfiles
-cd "${DOTFILES_ROOT}" || exit
+cd "${DOTFILES}" || exit
 
-find "${DOTFILES_ROOT}" -type f ! -path '*/.git/*' ! -path '*/link.sh'
-
+find "${DOTFILES}" -type f ! -path '*/.git/*' ! -path '*/link.sh' -print0 | \
+	while IFS= read -r -d $'\0' source; do
+		target="$HOME/${source#$DOTFILES/}"
+		if [[ ! -e "${target}" ]]; then
+			ln -s "${source}" "${target}"
+			echo "create link ${source} -> ${target}"
+		elif [[ -L "${target}" ]]; then
+			echo "skip existing symlink ${target}"
+		else
+			echo "WARNING: ${target} exists and is not a symlink, manual fix required."
+		fi
+	done
